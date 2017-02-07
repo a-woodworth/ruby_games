1f4e95e26a95b06ec271339d752428bb4b938611
Amy Woodworth <woodworthcommunications@gmail.com>
2017-02-06 19:58:41 -0600

    Start work on SectorFive game.

---
+++ SectorFive/sector_five.rb
@@ -0,0 +1,27 @@
+#---
+# Excerpted from "Learn Game Programming with Ruby",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/msgpkids for more book information.
+#---
+require 'gosu'
+require_relative 'player'
+
+class SectorFive < Gosu::Window
+
+  def initialize
+    super(800, 600)
+    self.caption = 'Sector Five'
+    @player = Player.new(self)
+  end
+
+  def draw
+    @player.draw
+  end
+
+end
+
+window = SectorFive.new
+window.show
